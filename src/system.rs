fd68ffbab18fa3dcd43b0a8d305a58266c907e69
Alex Laurent <shaoner@pm.me>
2022-05-13 15:55:50 -0400

    Add speaker and APU support to system

--- src/system.rs
+++ src/system.rs
@@ -1,13 +1,16 @@
 use core::ops::Deref;
 use core::time::Duration;
 
-use crate::{Button, Error, Rom, Screen, SerialOutput};
+use crate::{Button, Error, Rom, Screen, AudioSpeaker, SerialOutput};
 use crate::bus::Bus;
 use crate::cpu::{Cpu, CLOCK_SPEED};
 
 pub const DEFAULT_FRAME_RATE: u32 = 60;
 
-pub struct System<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> {
+pub struct System<T: Deref<Target=[u8]>,
+                  S: Screen,
+                  SO: SerialOutput,
+                  AS: AudioSpeaker> {
     /// Address bus
     bus: Bus<T>,
     /// To execute instructions
@@ -16,12 +19,17 @@ pub struct System<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> {
     screen: S,
     /// A serial output to give to the serial controller
     serial_output: SO,
+    /// An audio speaker interface
+    speaker: AS,
     /// Keep the number of cycles before a frame is refreshed
     cycles_per_frame: u32,
 }
 
-impl<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> System<T, S, SO> {
-    pub fn new(rom: Rom<T>, screen: S, serial_output: SO) -> Self {
+impl<T: Deref<Target=[u8]>,
+     S: Screen,
+     SO: SerialOutput,
+     AS: AudioSpeaker> System<T, S, SO, AS> {
+    pub fn new(rom: Rom<T>, screen: S, serial_output: SO, speaker: AS) -> Self {
         let bus = Bus::new(rom);
         let cpu = Cpu::new();
 
@@ -30,6 +38,7 @@ impl<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> System<T, S, SO> {
             cpu,
             screen,
             serial_output,
+            speaker,
             cycles_per_frame: CLOCK_SPEED / DEFAULT_FRAME_RATE,
         }
     }
@@ -63,6 +72,7 @@ impl<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> System<T, S, SO> {
         let ticks = self.cpu.step(&mut self.bus);
 
         for _ in 0..ticks {
+            self.bus.apu.step(&mut self.speaker);
             self.bus.ppu.step(&mut self.screen, &mut self.bus.it);
             self.bus.timer.step(&mut self.bus.it);
         }
@@ -89,11 +99,22 @@ impl<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> System<T, S, SO> {
         &mut self.serial_output
     }
 
+    /// Retrieve the speaker
+    pub fn speaker(&mut self) -> &mut AS {
+        &mut self.speaker
+    }
+
     /// Forward a button press to the joypad controller
     /// ```
-    /// # use padme_core::{Button, Pixel, Rom, Screen, SerialOutput, System};
+    /// # use padme_core::*;
     /// # use std::thread::sleep;
     /// # use std::time::Instant;
+    /// #
+    /// # struct MyAudioPlayer;
+    /// #
+    /// # impl AudioSpeaker for MyAudioPlayer {
+    /// #     fn set_samples(&mut self, _left: f32, _right: f32) { }
+    /// # }
     /// #
     /// # struct Lcd;
     /// #
@@ -111,7 +132,7 @@ impl<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> System<T, S, SO> {
     /// #
     /// # let mut bin = [0u8; 32 * 1024];
     /// # let mut rom = Rom::load(&mut bin[..]).unwrap();
-    /// let mut emu = System::new(rom, Lcd, Console);
+    /// let mut emu = System::new(rom, Lcd, Console, MyAudioPlayer);
     /// emu.set_button(Button::A, true);
     /// emu.set_button(Button::Up, true);
     /// ```
@@ -128,9 +149,15 @@ impl<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> System<T, S, SO> {
 
     /// Execute enough steps to retrieve 1 frame
     /// ```
-    /// # use padme_core::{Button, Pixel, Rom, Screen, SerialOutput, System};
+    /// # use padme_core::*;
     /// # use std::thread::sleep;
     /// # use std::time::Instant;
+    /// #
+    /// # struct MyAudioPlayer;
+    /// #
+    /// # impl AudioSpeaker for MyAudioPlayer {
+    /// #     fn set_samples(&mut self, _left: f32, _right: f32) { }
+    /// # }
     /// #
     /// # struct Lcd;
     /// #
@@ -148,7 +175,7 @@ impl<T: Deref<Target=[u8]>, S: Screen, SO: SerialOutput> System<T, S, SO> {
     /// #
     /// # let mut bin = [0u8; 32 * 1024];
     /// # let mut rom = Rom::load(&mut bin[..]).unwrap();
-    /// let mut emu = System::new(rom, Lcd, Console);
+    /// let mut emu = System::new(rom, Lcd, Console, MyAudioPlayer);
     /// // loop {
     ///     let t0 = Instant::now();
     ///     emu.update_frame();
